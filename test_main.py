72b4ecf883aa3058a63fb1aa8314a1d97d01394e
t-T-s <35894374+t-T-s@users.noreply.github.com>
2023-11-08 12:55:12 +0000

    [Test] Updated test_main with basic consistency endpoint test

--- test_main.py
+++ test_main.py
@@ -6,7 +6,7 @@ from main import app
 client = TestClient(app)
 
 
-def test_get_accuracy_1d():
+def test_post_accuracy_1d():
     # Inputs as 1d arrays for accuracy check
     item_data = {"ground_truth": [1, 2, 2, 4, 5],
                  "predictions": [1, 2, 3, 4, 5]}
@@ -15,7 +15,7 @@ def test_get_accuracy_1d():
     assert response.json() == {"accuracy": 0.80}
 
 
-def test_get_accuracy_label_indicator():
+def test_post_accuracy_label_indicator():
     # Inputs as label indicator arrays. There can be multiple columns
     item_data = {"ground_truth": [[0, 1], [1, 0]],
                  "predictions": [[0, 1], [1, 1]]}
@@ -24,7 +24,7 @@ def test_get_accuracy_label_indicator():
     assert response.json() == {"accuracy": 0.50}
 
 
-def test_get_accuracy_label_indicator_inner_list_lengths():
+def test_post_accuracy_label_indicator_inner_list_lengths():
     # Inputs as label indicator arrays of different nested list lengths.
     item_data = {"ground_truth": [[0, 1], [1, 1], [1]],
                  "predictions": [[0, 1], [1, 1], [0]]}
@@ -32,12 +32,22 @@ def test_get_accuracy_label_indicator_inner_list_lengths():
     assert response.status_code != 200
 
 
-def test_get_accuracy_1d_length():
+def test_post_accuracy_1d_length():
     # Inputs as 1d arrays for accuracy check
     item_data = {"ground_truth": [1, 2, 2, 4, 5, 8],
                  "predictions": [1, 2, 3, 4, 5]}
     response = client.post("/clf_accuracy", json=item_data)
     assert response.status_code != 200
+
+
+def test_post_consistency_scores():
+    # Basic invocation test
+    contrib_dict = {'KernelSHAP': [[0.15, 0.2], [0.3, 0.42]],
+                    'SamplingSHAP': [[0.12, 0.2], [0.32, 0.4]],
+                    'LIME': [[0.14, 0.2], [0.34, 0.4]]}
+    response = client.post("/consistency", json=contrib_dict)
+    assert response.status_code == 200
+    assert response.json().keys() == {"average_consistency", "pairwise_scores"}
 
 
 if __name__ == '__main__':
